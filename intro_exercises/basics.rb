181da1449c6c86a9d77ffb5f14b0ba835e6ac060
Laura <lauraslyons@gmail.com>
2016-11-16 23:13:26 +0000

    moved file

---
+++ intro_exercises/basics.rb
@@ -0,0 +1,11 @@
+#! /usr/bin/env ruby
+first = "Laura"
+last = " Lyons"
+puts first + last
+
+thousands = 1234 / 1000
+puts thousands
+puts 1234 % 1000
+puts 1234 % 1000 / 100
+puts 1234 % 1000 % 100 / 10
+puts 1234 % 1000 % 100 % 10
