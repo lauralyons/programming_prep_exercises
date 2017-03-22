51f37e42ccff1b6d01d7ed1c8f3eebe242f852b9
Laura <lauraslyons@gmail.com>
2017-03-22 22:08:18 +0000

    add files

--- intro_exercises/countdown.rb
+++ intro_exercises/countdown.rb
@@ -1,7 +1,12 @@
-x = gets.chomp.to_i
-
-while x >= 0
-puts x
-x = x -= 1
+def count_to_zero(number)
+  if number <= 0
+    puts number
+  else
+    puts number
+    count_to_zero(number-1)
+  end
 end
-puts "done"
+
+count_to_zero(10)
+count_to_zero(20)
+count_to_zero(-3)
