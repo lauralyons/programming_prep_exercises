3203d398a737f6567859dea1d26b85555d678337
Laura <lauraslyons@gmail.com>
2017-04-28 02:30:54 +0000

    new files

---
+++ intro_exercises/hashes.rb
@@ -0,0 +1,15 @@
+
+person = {name: 'bob', height: '6 ft', weight: '160 lbs', hair: 'brown'}
+
+person.each do |key, value|
+  puts "Bob's #{key} is #{value}"
+end
+
+person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+puts person[:name]
+
+puts person.has_value?("Bob")
+
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+          'flow', 'neon']
